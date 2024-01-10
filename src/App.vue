db7e157f711e83116b7c6bd2637708bafb81e17c
dalwa <dalwaifahad@gmail.com>
2024-01-10 10:28:32 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>To-Do List</h1>
+    <to-do-form @todo-added="addToDo"></to-do-form>
+    <ul>
+      <li v-for="item in ToDoItems" :key="item.id"> //For each item in the the array its making an instance of todo
+        <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import toDoItem from "./components/ToDoItem.vue"
+import toDoForm from "./components/ToDoForm.vue"
 
 export default {
-  name: 'App',
+  
+  name: "app",
   components: {
-    HelloWorld
+    toDoItem,
+    toDoForm
+  },
+  
+  data(){
+    return{
+      ToDoItems:[
+        {id:crypto.randomUUID(),label:"Learn Vue",done:true},
+        {id:crypto.randomUUID(),label:"Learn Go",done:false},
+        {id:crypto.randomUUID(),label:"Make loging authentication page",done:false},
+        {id:crypto.randomUUID(),label:"Finish Leetcode questions",done:false},
+      ]
+    };
+
+  },
+  methods:{
+  addToDo(toDoLabel){
+    this.ToDoItems.push({id:crypto.randomUUID(), label: toDoLabel, done: false});
   }
 }
+  
+};
+
 </script>
 
 <style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <input type="checkbox" :id="id" :checked="isDone"/>
+        <label :for="id">{{label}}</label>
+    </div>
+</template>
+
+<script>
+
+export default{
+    props:{ //props are how you pass down data from parent to component(1 way)
+        label:{required:true,type:String},
+        done:{default:false,type:Boolean},
+        id:{required:true,type:String}
+    },
+    data(){ //Used to store and alter variables(2 way)
+        return{
+            isDone:this.done, //isDone variable takes the value of the done prop
+        }
+    }
+};
+</script>
